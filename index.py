33312b614cf5e793787ed0f25903fdb8c6e55574
Zakariya Sattar <zakariyasattar03@gmail.com>
2020-05-11 12:46:09 -0500

    finish with submit_buy

--- index.py
+++ index.py
@@ -6,16 +6,24 @@ import alpaca_trade_api as tradeapi
 from iexfinance.stocks import Stock
 import json
 import re
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import numpy as np
 
 """
     TODO:
 
     Finish decide
-    Test submitOrder
-    Finish getQuote
 """
 
+# Init Webdriver
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+options.add_argument("--log-level=3")
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver.set_window_position(-10000,0)
 
 # ALPACA API INIT
 api = tradeapi.REST('PKWS07EUH6HN0TVT14O2', 'A5Ffk2KHJ6UO10xiEqwrsRzh4G68e5Am3OlQ421m', "https://paper-api.alpaca.markets", api_version='v2')
@@ -23,9 +31,9 @@ alphavantage_api_key = "L9ORMXCSUYTKB325"
 iex_api_key = "Tpk_4bd3eca6cc5d45418180c6881292b18e"
 
 # pull data from cboe: https://markets.cboe.com/us/equities/market_statistics/book/?mkt=edgx
-CBOE_URL = 'https://markets.cboe.com/us/equities/market_statistics/book/?mkt=edgx'
-page = requests.get(CBOE_URL)
-cboe_data = BeautifulSoup(page.content, 'html.parser')
+driver.get("https://markets.cboe.com/us/equities/market_statistics/book/?mkt=edgx")
+html = driver.execute_script('return document.body.innerHTML;')
+cboe_data = BeautifulSoup(html, 'lxml')
 
 # Populate bids and asks arrays with order size data
 def populateAsksBids(bid_share_count, bid_price, ask_share_count, ask_price):
@@ -62,20 +70,19 @@ def getQuote(symbol):
     # replace sandbox with cloud and replace api key
     iex_url = "https://sandbox.iexapis.com/stable/stock/" + symbol + "/quote?token=" + iex_api_key
     iex_data = requests.get(iex_url).content
-    price_data = iex_data.decode('utf8').replace("'", '"')
-    data = json.loads(price_data)
-    s = json.loads(json.dumps(data, indent=4, sort_keys=True))
-    return (s["latestPrice"])
+
+    price_data = json.loads(json.dumps(json.loads(iex_data.decode('utf8').replace("'", '"')), indent=4, sort_keys=True))
+    return (price_data["latestPrice"])
 
 def submitOrder(side, ticker):
-    buying_power = (float(api.get_account().buying_power)) - 1000
-    api.submit_order(ticker, round(buying_power / getQuote(ticker)), side, "market", "day")
-    # print("Market order of | " + round(float(api.get_account().buying_power) / getQuote(ticker)) + " " + ticker + " " + side + " | completed.")
+    buying_power = (float(api.get_account().buying_power))
+    qty = round(buying_power / getQuote(ticker))
+
+    api.submit_order(ticker, qty, side, "market", "day")
+    print("Market order of | " + str(qty) + " " + ticker + " " + side + " | completed.")
 
 
-# while True:
-#     if(api.get_clock().is_open):
-#         populateAsksBids(bid_share_count, bid_price, ask_share_count, ask_price)
-#     time.sleep(5)
-
-print(getQuote("INO"))
+while True:
+    if(api.get_clock().is_open):
+        populateAsksBids(bid_share_count, bid_price, ask_share_count, ask_price)
+    time.sleep(5)
